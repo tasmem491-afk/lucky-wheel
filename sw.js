5c3feb37cf2ab45ae20ce1a080d1387c7bfce6fc
tasmem491-afk <tasmem491@gmail.com>
2026-02-02 14:33:33 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const cacheName = 'lucky-wheel-v1';
+const assets = [
+  './',
+  './index.html',
+  './icon.png'
+];
+
+// تثبيت ملفات الكاش
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// جلب البيانات من الكاش عند انقطاع الشبكة
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
